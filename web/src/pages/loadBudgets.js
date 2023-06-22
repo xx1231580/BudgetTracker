4ed9e8f0ef3ef6ad3b3d8ffc43f4df4f85170b25
Tyler <mm1231580@gmail.com>
2023-06-22 11:44:10 -0500

    small cleanup

--- web/src/pages/loadBudgets.js
+++ web/src/pages/loadBudgets.js
@@ -3,16 +3,13 @@ import Header from '../components/header';
 import BindingClass from "../util/bindingClass";
 import DataStore from "../util/DataStore";
 
-/**
- * Logic needed for the view playlist page of the website.
- */
+
 class LoadBudgets extends BindingClass {
     constructor() {
         super();
         this.bindClassMethods(['clientLoaded', 'mount', 'addBudgetsToPage', 'createTable'], this);
         this.dataStore = new DataStore();
         this.dataStore.addChangeListener(this.addBudgetsToPage);
-        //this.dataStore.addChangeListener(this.addExpensesToPage);
         this.header = new Header(this.dataStore);
     }
 
@@ -21,7 +18,7 @@ class LoadBudgets extends BindingClass {
             return '<h4>No results found</h4>';
         }
 
-        let html = '<table><tr><th>Project Title</th><th>Status</th><th>Actions</th></tr>';
+        let html = '<table><tr><th>Budget</th><th>Status</th><th>Actions</th></tr>';
         for (const res of budgets) {
             html += `
             <tr>
@@ -39,42 +36,32 @@ class LoadBudgets extends BindingClass {
         return html;
     }
 
-    /**
-     * Once the client is loaded, get the playlist metadata and song list.
-     */
+    
     async clientLoaded() {
-        //const projectId = urlParams.get('projectId');
-        //document.getElementById('project-title').innerText = "Loading Project ...";
+      
         const budgets = await this.client.getAllBudgets();
         this.dataStore.set('budgets', budgets);
-        //document.getElementById('tickets').innerText = "(loading tickets...)";
-        //const tickets = await this.client.getProjectTickets(projectId);
-        //this.dataStore.set('tickets', tickets);
+        
         console.log("budgets", budgets);
     }
 
-    /**
-     * Add the header to the page and load the TicketTrackerClient.
-     */
+  
     mount() {
-        //document.getElementById('add-ticket').addEventListener('click', this.addTicket);
+       
         this.header.addHeaderToPage();
 
         this.client = new MusicPlaylistClient();
         this.clientLoaded();
     }
 
-    /**
-     * When the playlist is updated in the datastore, update the playlist metadata on the page.
-     */
+   
     addBudgetsToPage() {
         const budgets = this.dataStore.get('budgets');
         if (budgets == null) {
             return;
         }
 
-        //document.getElementById('project-title').innerText = project.title;
-        //document.getElementById('project-description').innerText = project.description;
+        
 
         let expenseHtml = '';
         let budgetId;
